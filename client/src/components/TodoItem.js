c4df9bc26edefa0ad80ffc73bb52861f18044aec
Rasmus Nordling <rasmus.nordling@gmail.com>
2021-08-08 03:42:48 +1000

    Add edit functionality to TodoItem

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -3,6 +3,22 @@ import React, { Component } from "react";
 import styles from "./TodoItem.module.css";
 
 class TodoItem extends Component {
+  state = {
+    editing: false,
+  }
+  
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  }
+
+  handleUpdateDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false });
+    }
+  };
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -13,19 +29,39 @@ class TodoItem extends Component {
 
     // Obtain variables by Destructing the object
     const { id, title, completed } = this.props.todo
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none";
+    } else {
+      editMode.display = "none";
+    }
 
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type = "checkbox"
+            className={styles.checkbox}
+            checked = {completed}
+            onChange = {() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)} className={styles.itemButton}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
         <input
-          type = "checkbox"
-          className={styles.checkbox}
-          checked = {completed}
-          onChange = {() => this.props.handleChangeProps(id)}
+          type = "text"
+          style = {editMode}
+          className = {styles.textInput}
+          value = {title}
+          onChange = {e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdateDone}
         />
-        <button onClick={() => this.props.deleteTodoProps(id)} className={styles.itemButton}>
-          Delete
-        </button>
-        <span style={completed ? completedStyle : null}>{title}</span>
       </li>
     )
   }
