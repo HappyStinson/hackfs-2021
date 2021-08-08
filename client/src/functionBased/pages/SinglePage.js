089de2efa856f0d5b70b781230f5670a5e61851c
Rasmus Nordling <rasmus.nordling@gmail.com>
2021-08-09 00:35:39 +1000

    Add content about the app and author to SinglePage

--- client/src/functionBased/pages/SinglePage.js
+++ client/src/functionBased/pages/SinglePage.js
@@ -3,8 +3,31 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const SinglePage = () => {
-  console.log(useParams());
-  return <div>Hello from single page!</div>
+  const aboutData = [
+    {
+      slug: "about-app",
+      title: "About the App",
+      description:
+        "My project for ETHGlobal HackFS 2021 Hackathon! A text-based NFT adventure game, introducing players to the blockchain. This game presents a text-based interface in the browser to allow players to engage in a story that will educate them on different topics around the blockchain. Players can trade and exchange items found in the game. It will feature music from the Audius API.",
+    },
+    {
+      slug: "about-author",
+      title: "About the Hacker",
+      description:
+        "Rasmus Nordling from 🇸🇪 Blockchain Developer & Musician!",
+    },
+  ];
+
+  const { slug } = useParams();
+  const aboutContent = aboutData.find(item => item.slug === slug);
+  const { title, description } = aboutContent;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  )
 };
 
 export default SinglePage
